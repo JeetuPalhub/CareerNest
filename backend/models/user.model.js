fbbdead6f8898d09fc79205605a6a88b4f589378
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-08-27 23:39:24 +0530

    user.model.js complete

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -23,7 +23,15 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     profile: {
-        bio: [{type: String}],
-        skills: [{type: String}]
-    }
-})
+        bio: {type: String},
+        skills: [{type: String}],
+        resume: {type: String},
+        resumeOriginalName: {type: String},
+        company: {type: mongoose.Schema.Types.ObjectId, ref:'Company'},
+        profilePhoto: {
+            type: String,
+            default: ""
+        }
+    },
+},{timestamps: true});
+export const User = mongoose.model('User', userSchema);

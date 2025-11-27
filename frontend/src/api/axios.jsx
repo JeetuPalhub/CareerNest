5338aee41151c39158c0474b8cf5dffb573236c3
Jeetu Pal <jeetupal.pal31@gmail.com>
2025-11-27 22:30:39 +0530

    Delete frontend directory

--- frontend/src/api/axios.jsx
+++
@@ -1,9 +0,0 @@
-// src/api/axios.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:8000/api", // adjust to your backend URL
-  withCredentials: true, // IMPORTANT: send/receive cookies
-});
-
-export default api;
